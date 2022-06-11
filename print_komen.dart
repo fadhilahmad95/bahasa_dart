23aa6aa66f26c0297b62f7dc3c9fc14e09dec06c
dhilco_ <fadhilahmad95@gmail.com>
2022-06-11 15:18:55 +0800

    ini-print-coment

---
+++ print_komen.dart
@@ -0,0 +1,19 @@
+void main() {
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+  print("Name : $name"); // ini komen yang tidak dijalankan program
+  print("Tahun : ${year}");
+  
+  /*print(antennaDiameter);
+  print("Daftar Planet : ${flybyObjects[1]}");
+  print(image);*/
+
+  /*komen dengan banyak yang mau tidak dijalankan pada program */
+}
