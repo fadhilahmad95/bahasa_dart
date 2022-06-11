257de3befd7a187e54a3b0690918b46742792b02
dhilco_ <fadhilahmad95@gmail.com>
2022-06-11 16:22:03 +0800

    tipe-string

---
+++ tipe-string.dart
@@ -0,0 +1,16 @@
+void main() {
+  String nama ="Muhammad Fadhil";
+  String daftarSiswa = "Azis, Bambang, Cecep";
+  var nilaiSiswa = 75;
+
+  print(nama.contains("M", 1));
+
+  print(nama.toLowerCase());
+  print(nama.toUpperCase());
+
+  print("Nilai Anda : " + nilaiSiswa.toString());
+
+  print(daftarSiswa.split(","));
+
+  print(nama.substring(9));
+}
