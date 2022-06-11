168c25ea813b394a489f5d9363dc6dcb8ec61974
dhilco_ <fadhilahmad95@gmail.com>
2022-06-11 15:10:20 +0800

    tipe-data-var

---
+++ tipedatavar.dart
@@ -0,0 +1,16 @@
+void main() {
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+  print("Name : $name");
+  print("Tahun : ${year}");
+  print(antennaDiameter);
+  print("Daftar Planet : ${flybyObjects[1]}");
+  print(image);
+}
