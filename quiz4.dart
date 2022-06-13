4dc402ace49cecdf6d6820cafdd97db6353447f1
dhilco_ <fadhilahmad95@gmail.com>
2022-06-13 14:00:29 +0800

    update-quiz

--- quiz4.dart
+++ quiz4.dart
@@ -1,18 +1,36 @@
+import 'dart:io';
+
 void main() {
-  int n = 2;
-  for (int i = 0; i < n; i++) {
-    var spasi = "";
+  // int n = 2;
+  // for (int i = 0; i < n; i++) {
+  //   var spasi = "";
+  //   for (int j = 0; j <= i; j++) {
+  //     spasi = spasi + "*";
+  //   }
+  //   print(spasi);
+  // }
+  // int m = 4;
+  // for (int i = 0; i < m; i++) {
+  //   var spasi = "";
+  //   for (int j = m; j > i; j--) {
+  //     spasi = spasi + "*";
+  //   }
+  //   print(spasi);
+  // }
+
+  int nilai = 5;
+  for (int i = 0; i < nilai; i++) {
     for (int j = 0; j <= i; j++) {
-      spasi = spasi + "*";
+      stdout.write("*");
     }
-    print(spasi);
+    print("");
   }
-  int m = 4;
-  for (int i = 0; i < m; i++) {
-    var spasi = "";
-    for (int j = m; j > i; j--) {
-      spasi = spasi + "*";
+
+  int a = 5;
+  for (int i = 0; i < a; i++) {
+    for (int j = a; j > i; j--) {
+      stdout.write("*");
     }
-    print(spasi);
+    print("");
   }
 }
