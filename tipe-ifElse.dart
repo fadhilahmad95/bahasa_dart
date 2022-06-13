fbab1ccb13965887735a5af907ebe29ba9d4255f
dhilco_ <fadhilahmad95@gmail.com>
2022-06-13 10:46:56 +0800

    if-else

---
+++ tipe-ifElse.dart
@@ -0,0 +1,24 @@
+void main() {
+  // kondisi -> true -> jalankan code true
+  // kondisi -> false -> jalankan code false
+
+  int angka = 6;
+
+  if (angka == 0) {
+    print("Bilangan Kosong");
+  } else {
+    if (angka % 2 == 0) {
+      print("Genap");
+    } else {
+      print("Ganjil");
+    }
+  }
+
+  String nama = "Dicoding";
+
+  if (nama == "Dicoding") {
+    print("Selamat Datang Kelas Dicoding");
+  } else {
+    print("Anda Tidak Memiliki Kelas");
+  }
+}
