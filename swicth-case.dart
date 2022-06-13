3ae3b1c5883b281435f1d39590df590efea0c5d4
dhilco_ <fadhilahmad95@gmail.com>
2022-06-13 10:57:06 +0800

    swict-case

---
+++ swicth-case.dart
@@ -0,0 +1,19 @@
+void main() {
+  String nilai = "D";
+
+  switch (nilai) {
+    case "A":
+      print("Sangat Baik");
+      break;
+    case "B":
+      print("Baik");
+      break;
+    case "C":
+      print("Cukup");
+      break;
+    case "D":
+      print("Kurang");
+      break;
+    default:
+  }
+}
